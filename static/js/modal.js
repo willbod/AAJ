6f3d48ce418ad08ae09d78b01853dfa15fbdcd48
willbod <willbodansky@gmail.com>
2016-11-10 11:11:40 -0500

    New icons for services offered, new modals introduced for mission, vision, about us

---
+++ static/js/modal.js
@@ -0,0 +1,15 @@
+$('#qsModal').on('shown.bs.modal', function () {
+  $('#qsInput').focus()
+})
+
+$('#mModal').on('shown.bs.modal', function () {
+  $('#mInput').focus()
+})
+
+$('#vModal').on('shown.bs.modal', function () {
+  $('#vInput').focus()
+})
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
